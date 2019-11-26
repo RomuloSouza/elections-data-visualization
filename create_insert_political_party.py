3588f2676fc70fb67061b79831fecbae58cf807f
Vinicius Lima <vfbdelima@gmail.com>
2019-11-26 00:44:03 -0300

    Add script to populate political parties
    Signed-off-by: Vinicius Lima <vfbdelima@gmail.com>

---
+++ create_insert_political_party.py
@@ -0,0 +1,45 @@
+import pandas as pd
+
+INSERT_PARTY = """
+INSERT INTO PARTIDO (sigla, nomePartido)
+VALUES ('{}', '{}');
+"""
+
+
+def create_insert_string(row):
+    nome_partido = row[1]
+    sigla_partido = row[2]
+
+    insert_sql = INSERT_PARTY.format(
+        sigla_partido,
+        nome_partido
+    )
+    
+    return insert_sql
+
+
+def add_lines_to_file(lines):
+    f = open('./sql_scripts/popula_partidos.sql', 'w+')
+    f.write('USE eleicoes;\n')
+    for line in lines:
+        f.write(line)
+
+    f.close()
+
+
+if __name__ == '__main__':
+    filename = 'candidatura.csv'
+
+    data = pd.read_csv(filename, usecols=['nome_partido', 'sigla_partido'])
+    data = data.dropna(subset=['nome_partido', 'sigla_partido'])
+    data = data.drop_duplicates(subset=['nome_partido', 'sigla_partido'])
+    counter = 0
+    lines = []
+    for row in data.itertuples():
+        line = create_insert_string(row)
+        lines.append(line)
+
+        counter += 1
+
+    print('tamanho = ', counter)
+    add_lines_to_file(lines)
